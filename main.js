f8838bd77d903ab8bd0387359eec17220cf4b0f0
unknown <kzaid2918@gmail.com>
2025-12-10 23:13:47 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,90 @@
+// var myName = prompt("enter your name");
+
+// var myNameLentgh = myName.length;
+// if (myNameLentgh > 3) {
+//   var result = myName.slice(0, 3);
+//   console.log("result=>", result);
+// }
+
+// var city = prompt("enter your city name");
+// var mycityname = city.length;
+// if(mycityname>3){
+//   var result = city.slice(0,3)
+//   console.log(result)
+// }
+
+                 
+
+// var cityName = prompt("enter city name")
+// var cityNames = cityName.length;
+// for (i = 0; i < cityNames; i++) {
+//   if (cityName.slice(i, i+2) === "  ") {
+//     console.log("hey , double space not allowed");
+//     break
+//   } else {
+//     console.log("there is no double space try again");
+//   }
+// }
+
+// var city = prompt("enter your password");
+ 
+// var correctPassword = city==="zaid444abc";
+// for (i=0; i<1; i++) { if(correctPassword){
+//       console.log("You have successfully cracked the password");
+
+// }
+// if(city==="a" || "b" || "c" || "d" || "e" || "f" || "g" || "h" || "i" || "j" || "k" || "l" || "m" || "n" || "o" || "p" || "q" || "r" || "s" || "t" || "u" || "v" || "w" || "x" || "y" || "z" || 0 || 1 || 2 || 3 || 4 || 5 || 6 || 7 || 8 || 9)
+//   { console.log("Incorrect password try again");
+    
+//      }
+//      if(city="+" || "-" || "%" || "*"){
+//       console.log("You cant use this in your password")
+//     }
+//     }
+
+// var cityName = "This is World War II story of the Past World War II";
+// var cityNames = cityName.length;
+// for (i = 0; i < cityNames; i++) {
+//   if (cityName.slice(i, i + 12) === "World War II") {
+//     cityName =
+//       cityName.slice(0, i) + "The scond World War" + cityName.slice(i + 12);
+//       console.log("cityName =>", cityName);
+//     }
+// }
+
+// var name = "ibrahim"
+// var check = name.indexOf("j")
+// console.log(check)
+
+// var sentence = "This is beautiful Pakistan in Asia agian Pakistan";
+// var sentences = sentence.lastIndexOf("Pakistan");
+// if (sentences !== -1) {
+//   var result =
+//     sentence.slice(0, sentences) + "Thailand" + sentence.slice(sentences + 8);
+//   console.log(result);
+// }
+
+// var sentence = "This is beautiful Pakistan in Asia agian Pakistan Pakistan";
+// // var reult = sentence.replace(/Pakistan/g,"thailand")
+// var reult = sentence.replaceAll("Pakistan","thailand")
+// console.log(reult)
+
+// var name = "absdfjhdskjfhdjkhbdjkvc";
+// var names = name.charAt(name.length - 1);
+// if (names == "c") {
+//   var upper = names.toUpperCase();
+//   console.log(upper, "found");
+// } else {
+//   console.log("not found");
+// }
+
+// var city = "pak!istan";
+
+// for (i = 0; i < city.length; i++) {
+//   if (city.charAt(i) == "!") {
+//     console.log("found at " + i + " Index " + city);
+//   }
+// }
+
+// var result = city.charAt(0)
+// console.log(result)
